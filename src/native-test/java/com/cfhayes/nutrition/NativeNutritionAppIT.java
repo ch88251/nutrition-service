a137bdf497de9d944827c7a52dd25d99261377b6
chayes <cfhayes16@gmail.com>
2020-11-26 17:59:08 -0700

    Initial project setup

---
+++ src/native-test/java/com/cfhayes/nutrition/NativeNutritionAppIT.java
@@ -0,0 +1,9 @@
+package com.cfhayes.nutrition;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeNutritionAppIT extends NutritionAppTest {
+
+    // Execute the same tests but in native mode.
+}
